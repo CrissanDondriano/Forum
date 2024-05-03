8fe18a04054fd65c31a9c58a9d0fc96caf000be6
Crissan Dondriano <dondrianocrissan20@gmail.com>
2024-05-03 20:12:21 +0800

    back to have no auth authenticate

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\AuthController;
@@ -11,19 +12,16 @@ Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::get('/registration', [AuthController::class, 'registration'])->name('registration');
 Route::post('/register-user', [AuthController::class, 'registerUser'])->name('register-user');
 Route::post('/login-user', [AuthController::class, 'loginUser'])->name('login-user');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+// Route for Posts
+Route::get('/posts/create', function() {
+    return view('posts.create');
+})->name('posts.create');
 
-Route::middleware(['auth'])->group(function () {
-    Route::get('/posts/create', function() {
-        return view('posts.create');
-    })->name('posts.create');
-
-    Route::get('/posts', [PostController::class, 'index'])->name('home');
-    Route::post('/create', [PostController::class, 'store'])->name('create');
-    Route::get('/show/{id}', [PostController::class, 'show'])->name('show');
-    Route::get('/edit/{id}', [PostController::class, 'edit'])->name('edit');
-    Route::put('/update/{id}', [PostController::class, 'update'])->name('update');
-    Route::delete('/delete/{id}', [PostController::class, 'destroy'])->name('delete');
-});
-
+Route::get('/posts', [PostController::class, 'index'])->name('home');
+Route::post('create', [PostController::class, 'store'])->name('create');
+Route::get('show/{id}', [PostController::class, 'show'])->name('show');
+Route::get('edit/{id}', [PostController::class, 'edit'])->name('edit');
+Route::put('update/{id}', [PostController::class, 'update'])->name('update');
+Route::delete('delete/{id}', [PostController::class, 'destroy'])->name('delete');
 
